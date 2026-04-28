d2087bc221c31d6080025b4b1b72359f8ed957dd
elysenicole <enave1@lsu.edu>
2026-04-28 15:41:48 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -14,7 +14,7 @@
  */
 function createPhotoMap () {
   // URL of a Google Sheets spreadsheet output as CSV
-  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSuESp280YE8_PokNYJSK_V3zxGh7ZJlDqMNeliCoz1kt6q2ii7lRovZ-yMlEFY2n6rhUu3vOpnvFGx/pub?gid=0&single=true&output=csv';
+  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRMl5abAeN8jC_HVRvgKp9O1HXosIoKbVc1tJxBF9vN6Vjke9EqBrL78xz3NdUyz3JMDfKiFEGyl7Mc/pub?gid=0&single=true&output=csv;
   
   // create map object with center lat/lon and zoom level
   var map = L.map('map').setView([30.44, -91.187], 13);
